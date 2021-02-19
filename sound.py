f7669b2d9624ace4702ef76a3617a3d1aed15d52
Lorenzo <58704770+LorenzoJacques@users.noreply.github.com>
2021-02-19 10:17:06 +0100

    Enlevé des restes de debug de sound.py

--- sound.py
+++ sound.py
@@ -4,8 +4,3 @@ charge=pygame.mixer.Sound("ressources//sound//charge.wav")
 bug=pygame.mixer.Sound("ressources//sound//bug.wav")
 activation=pygame.mixer.Sound("ressources//sound//activation.wav")
 loop=pygame.mixer.Sound("ressources//sound//loop.wav")
-
-print(activation.get_length())
-
-
-
