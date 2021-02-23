6d317693295e58b752fda1f6f8eec2f9dbb3e820
Lorenzo <58704770+LorenzoJacques@users.noreply.github.com>
2021-02-23 23:30:23 +0100

    changed sound.py to handle script folder

--- script/sound.py
+++ script/sound.py
@@ -1,6 +1,6 @@
 import pygame
 
-charge=pygame.mixer.Sound("ressources//sound//charge.wav")
-bug=pygame.mixer.Sound("ressources//sound//bug.wav")
-activation=pygame.mixer.Sound("ressources//sound//activation.wav")
-loop=pygame.mixer.Sound("ressources//sound//loop.wav")
+charge=pygame.mixer.Sound("..//ressources//sound//charge.wav")
+bug=pygame.mixer.Sound("..//ressources//sound//bug.wav")
+activation=pygame.mixer.Sound("..//ressources//sound//activation.wav")
+loop=pygame.mixer.Sound("..//ressources//sound//loop.wav")
