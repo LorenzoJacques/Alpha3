0c5631c99363e564945d80515115cfb9bbff2ad6
Lorenzo <58704770+LorenzoJacques@users.noreply.github.com>
2021-02-25 21:18:54 +0100

    Update cache.py

--- script/cache.py
+++ script/cache.py
@@ -138,5 +138,5 @@ load_rotate(pygame.image.load("..//ressources//anim//selector//Selector_201.png"
 load_charge((100,100),"point",concentration=5,speed_of_ball=30)
 load_decharge((300,300),"center",concentration=5,speed=90)
 
-#load_charge((600,600),"ending",ball_alpha=True,concentration=20,duration=150,speed_of_ball=60)
+load_charge((600,600),"ending",ball_alpha=True,concentration=20,duration=150,speed_of_ball=60)
 load_grow(pygame.image.load("..//ressources//anim//grow//Ball.png").convert_alpha(),"ending",(1000,1000),60)
