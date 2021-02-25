17acb42e15502e66b98f74748350edf26df4a7ae
Lorenzo <58704770+LorenzoJacques@users.noreply.github.com>
2021-02-25 22:27:57 +0100

    Update cache.py

--- script/cache.py
+++ script/cache.py
@@ -140,4 +140,4 @@ load_charge((100,100),"point",concentration=5,speed_of_ball=30)
 load_decharge((300,300),"center",concentration=5,speed=90)
 
 load_charge((500,500),"ending",ball_alpha=True,concentration=15,duration=150,speed_of_ball=70)
-load_grow(pygame.image.load("..//ressources//anim//grow//medium.png").convert_alpha(),"ending",(10,10),120)
+load_grow(pygame.image.load("..//ressources//anim//grow//medium.png").convert_alpha(),"ending",(10,10),90)
