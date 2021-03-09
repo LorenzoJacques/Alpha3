5facf0e2c4bd8bccabd49f1aea8508b84083af23
Lorenzo <58704770+LorenzoJacques@users.noreply.github.com>
2021-03-09 19:57:15 +0100

    Update sound.py

--- script/sound.py
+++ script/sound.py
@@ -1,5 +1,5 @@
 import pygame
-
+pygame.mixer.init(48000, -16, 1, 1024)
 charge=pygame.mixer.Sound("..//ressources//sound//charge.wav")
 bug=pygame.mixer.Sound("..//ressources//sound//bug.wav")
 activation=pygame.mixer.Sound("..//ressources//sound//activation.wav")
