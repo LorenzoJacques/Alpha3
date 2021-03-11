40ca8f7343ca12a52f3ad6651b9b876082d223bc
Lorenzo <58704770+LorenzoJacques@users.noreply.github.com>
2021-03-11 21:17:31 +0100

    Update data.py

--- script/data.py
+++ script/data.py
@@ -57,6 +57,7 @@ ending_text=["Du vide à la lumière",
 " ",
 "Nicolas Flamel"]
 
-ending_image=pygame.Surface((500,500))
+ending_image=pygame.Surface((250,250))
+ending_image.set_alpha(0)
 for i in range(0,len(ending_text)) :
-    ending_font.render_to(ending_image,(0,i*10),ending_text[i],fgcolor=pygame.Color(0,0,0),bgcolor=pygame.Color(255,255,255),size=20)
+    ending_font.render_to(ending_image,(len(ending_text[i])/2,i*20),ending_text[i],fgcolor=pygame.Color(0,0,0),bgcolor=pygame.Color(255,255,255),size=20)
