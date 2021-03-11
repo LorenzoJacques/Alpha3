caef45d06526e7fa7af60266f850a35f1c8391c4
Lorenzo <58704770+LorenzoJacques@users.noreply.github.com>
2021-03-11 21:13:10 +0100

    Update data.py

--- script/data.py
+++ script/data.py
@@ -59,4 +59,4 @@ ending_text=["Du vide à la lumière",
 
 ending_image=pygame.Surface((500,500))
 for i in range(0,len(ending_text)) :
-    ending_font.render_to(ending_image,(i*10,0),ending_text[i],fgcolor=pygame.Color(0,0,0),bgcolor=pygame.Color(255,255,255),size=20)
+    ending_font.render_to(ending_image,(0,i*10),ending_text[i],fgcolor=pygame.Color(0,0,0),bgcolor=pygame.Color(255,255,255),size=20)
