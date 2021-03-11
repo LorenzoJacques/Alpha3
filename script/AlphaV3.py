0cbe36a03a42219f600d518b6326fcbe152dee91
Lorenzo <58704770+LorenzoJacques@users.noreply.github.com>
2021-03-11 21:57:26 +0100

    i'm still a shitty programmer

--- script/AlphaV3.py
+++ script/AlphaV3.py
@@ -282,7 +282,7 @@ def animate_ending() :
 	if timer_on_win<5 :
 		sound.charge.play()
 	if timer_on_win==30 :
-		Layers['temp'].append(Animation("grow_ending",screen_mid,speed=1,anim_loop=True,begin_loop_at=145))
+		Layers['temp'].append(Animation("grow_ending",screen_mid,speed=1,anim_loop=True,begin_loop_at=119,end_loop_before=120))
 	if timer_on_win>120 :
 		CenterBlit(Screen,ending_image,settings.screen_mid)
 	timer_on_win=timer_on_win+1
